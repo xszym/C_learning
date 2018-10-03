4770fe208e90987af82304311e49028aa785bf64
SimonSpiceGears <xsz>
2018-10-03 23:31:01 +0200

    xszym
    Sum 2 ints, inputed by user

---
+++ ex2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+
+    printf("Podaj pierwsza liczbe przyjacielu: \n");
+    scanf("%d", &a);
+
+    printf("Podaj druga liczbe przyjacielu: \n");
+    scanf("%d", &b);
+
+    printf("Suma liczb to: %d\n", a + b);
+    return 0;
+}
