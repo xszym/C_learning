9d01c027a6b4ff41982a555bda9e56cad8feef87
xszym <xszym69@gmail.com>
2018-10-06 16:57:46 +0200

    Simple array, strcmp, if, strlen, gets

--- ey10.c
+++ ey10.c
@@ -7,10 +7,10 @@ int main(){
 
   printf("Give me a haslo mordeczko: ");
   //scanf("%s", &inputPassword); //It does not work
-  while(1){
-    gets(inputPassword);
-    !strcmp(password, inputPassword) ? printf("Zalogowany\n") : printf("Zle haslo\n");
-  }
+  gets(inputPassword);
+
+  !strcmp(password, inputPassword) ? printf("Zalogowany\n") : printf("Zle haslo\n");
+
   printf("Dlugosc hasla: %d\nDlugosc podanego hasla: %d\n", strlen(password), strlen(inputPassword));
 
   return 0;
