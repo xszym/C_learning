97bacdda303c6cd51fbe2d7bc2ef02320f64898b
xszym <xszym69@gmail.com>
2018-11-08 17:50:44 +0100

    Fibonacci in recursive funcion :fish_cake:  //TainCall update
    Conclusion is tail call is much faster like much much muczo

--- ey35.c
+++ ey35.c
@@ -5,19 +5,26 @@ nieujemnej liczby całkowitej n wartość elementu ciągu Fibonacciego
 o indeksie n.
 */
 
-int fibo(int n){
+//rekurencja
+unsigned long long int fibo(int n){
   int sum = 1;
-  
+
   if(n > 2) sum = fibo(n - 1) + fibo(n - 2);
 
   return sum;
+}
+
+//Rekurnecja ogodnowa :)
+unsigned long long int fiboTailCall(int n, int a, int b){
+  return n < 1 ? b : fiboTailCall(n - 1, a + b, a);
 }
 
 int main(){
   int a;
 
   scanf("%d", &a);
-  printf("%d", fibo(a));
+  printf("%llu, ", fiboTailCall(a, 1, 0));
+  printf("%llu, ", fibo(a));
 
   return 0;
 }
